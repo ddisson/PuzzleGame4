e5ccc355e790351d9534aea4bd88a01206705eb1
ddisson <ddisson89@gmail.com>
2025-03-18 10:56:40 +0800

    Initial Commit

---
+++ PuzzleGame4/PuzzleGame4App.swift
@@ -0,0 +1,20 @@
+//
+//  PuzzleGame4App.swift
+//  PuzzleGame4
+//
+//  Created by Dmitry Disson on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct PuzzleGame4App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
